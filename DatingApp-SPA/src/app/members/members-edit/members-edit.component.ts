ece92b581f85b08cb4594026ce019ede11f55aa4
ravindra K S <ravindra.ks7@gmail.com>
2019-03-12 11:13:06 +0530

    Updating resources on angular side as well

--- DatingApp-SPA/src/app/members/members-edit/members-edit.component.ts
+++ DatingApp-SPA/src/app/members/members-edit/members-edit.component.ts
@@ -3,6 +3,8 @@ import { ActivatedRoute } from '@angular/router';
 import { User } from 'src/app/_models/user';
 import { AlertifyjsService } from 'src/app/_service/alertifyjs.service';
 import { NgForm } from '@angular/forms';
+import { UserService } from 'src/app/_service/user.service';
+import { AuthService } from 'src/app/_service/auth.service';
 
 @Component({
   selector: 'app-members-edit',
@@ -19,7 +21,10 @@ export class MembersEditComponent implements OnInit {
     }
   }
 
-  constructor(private route: ActivatedRoute, private alertify: AlertifyjsService) { }
+  constructor(private route: ActivatedRoute,
+              private alertify: AlertifyjsService,
+              private userService: UserService,
+              private authServive: AuthService) { }
 
   ngOnInit() {
     this.route.data.subscribe(data => {
@@ -29,9 +34,12 @@ export class MembersEditComponent implements OnInit {
   }
 
   updateUser() {
-    console.log(this.user);
-    this.alertify.success('Profile updated successfully');
-    this.editForm.reset(this.user);
+    this.userService.updateUser(this.authServive.decodedToken.nameid, this.user).subscribe(next => {
+      this.alertify.success('Profile updated successfully');
+      this.editForm.reset(this.user);
+    }, error => {
+      this.alertify.error(error);
+    });
   }
 
 }
